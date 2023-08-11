7e01034bffba06281b23063872ac01c9e3b93280
sanjeev.pk <sanjeev.pk@gmail.com>
2023-08-12 01:19:19 +0530

    Added data.sql for on fly loading of the guru parampare

---
+++ src/main/resources/data.sql
@@ -0,0 +1,60 @@
+insert into yatigalu
+(
+   id,
+   aaradhane,
+   brindavana_place,
+   details,
+   peetadhipati,
+   period_from,
+   period_to
+)
+values
+(
+   1,
+   'Margashirsha Maasa, Krishna Paksha, Panchami',
+   'Mallkhed',
+   'Shri Shri 1008 Shri Akshobhya Teertha Shripadangalavaru',
+   'Shri Shri 1008 Shri Akshobhya Teertha Shripadangalavaru',
+   '1350',
+   '1365'
+);
+insert into yatigalu
+(
+   id,
+   aaradhane,
+   brindavana_place,
+   details,
+   peetadhipati,
+   period_from,
+   period_to
+)
+values
+(
+   2,
+   'Chaitra Maasa, Shukla Paksha, Pratipada',
+   'Kudli',
+   'Shri Shri 1008 Shri Thrailokya Bhushana Teertha Shripadangalavaru',
+   'Shri Shri 1008 Shri Thrailokya Bhushana Teertha Shripadangalavaru',
+   '1365',
+   '1366'
+);
+insert into yatigalu
+(
+   id,
+   aaradhane,
+   brindavana_place,
+   details,
+   peetadhipati,
+   period_from,
+   period_to
+)
+values
+(
+   3,
+   'Vaishaka Maasa, Shukla Paksha, Tritiya',
+   'Kudli',
+   'Shri Shri 1008 Shri Lokavanditha Teertha Sripadangalavaru',
+   'Shri Shri 1008 Shri Lokavanditha Teertha Sripadangalavaru',
+   '1366',
+   '1374'
+);
